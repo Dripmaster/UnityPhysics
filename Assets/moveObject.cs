1e0aaf9005fda0c0f61544a002ffa5dad82c2dec
Dripmaster <son342@gmail.com>
2020-06-11 17:37:15 +0900

    Create sample Collision Scene

---
+++ Assets/moveObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class moveObject : MonoBehaviour
+{
+    Rigidbody2D _rigidbody;
+    public Vector2 moveDir;
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), GetComponentsInChildren<BoxCollider2D>()[1]);
+    }
+
+    void FixedUpdate()
+    {
+        _rigidbody.MovePosition((Vector2)transform.position + moveDir * Time.deltaTime);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # UnityPhysics
 Test Unity Physics
+son342@gmail.com
+
+# 2020.06.11
+Create Sample Collision Scene
