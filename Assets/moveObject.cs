f4cfc81268a4a5d365fd8d599e5faa5fb4d0cc87
Dripmaster <son342@gmail.com>
2020-08-22 18:15:04 +0900

    사거리 표시 콜라이더

--- Assets/moveObject.cs
+++ Assets/moveObject.cs
@@ -9,9 +9,43 @@ public class moveObject : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody2D>();
         Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), GetComponentsInChildren<BoxCollider2D>()[1]);
     }
+    public bool isPlayer;
 
     void FixedUpdate()
     {
+        if (isPlayer)
+        {
+            Vector2 vel = Vector2.zero;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                vel.y += 1;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                vel.y -= 1;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                vel.x -= 1;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                vel.x += 1;
+            }
+
+            if (vel != Vector2.zero)
+            {
+                _rigidbody.MovePosition((Vector2)transform.position + vel * Time.deltaTime);
+            }
+        }
+        else
+            _rigidbody.MovePosition((Vector2)transform.position + moveDir * Time.deltaTime);
+    }
+    /*
+     * void FixedUpdate()
+    {
         _rigidbody.MovePosition((Vector2)transform.position + moveDir * Time.deltaTime);
     }
+    */
 }
